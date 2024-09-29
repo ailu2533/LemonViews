b02239fb39becf32a20d3cad83e5068bc26377f8
ailu <im.ailu@outlook.com>
2024-09-29 19:36:26 +0800

    update

--- Sources/LemonViews/BultinComponentWrapper/LemonScrollView.swift
+++ Sources/LemonViews/BultinComponentWrapper/LemonScrollView.swift
@@ -8,19 +8,23 @@
 import SwiftUI
 
 public struct LemonScrollView<Content: View>: View {
-    @ViewBuilder
+    let verticalMargin: CGFloat
+    let horizontalPadding: CGFloat
+
     var content: () -> Content
 
-    public init(@ViewBuilder content: @escaping () -> Content) {
+    public init(verticalMargin: CGFloat = 20, horizontalPadding: CGFloat = 16, @ViewBuilder content: @escaping () -> Content) {
         self.content = content
+        self.verticalMargin = verticalMargin
+        self.horizontalPadding = horizontalPadding
     }
 
     public var body: some View {
         ScrollView {
             content()
+                .padding(.horizontal, horizontalPadding)
         }
-        .contentMargins(.bottom, 20)
-        .contentMargins(16, for: .scrollContent)
+        .contentMargins(.vertical, verticalMargin)
         .scrollIndicators(.hidden)
     }
 }
