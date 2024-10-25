fb394606366769c8fcbf670327e6d2693d8298cc
ailu <im.ailu@outlook.com>
2024-10-25 22:38:26 +0800

    update

--- Sources/LemonViews/GeneralStatView.swift
+++ Sources/LemonViews/GeneralStatView.swift
@@ -8,33 +8,37 @@
 import SwiftUI
 
 public struct GeneralStatView<Item: Identifiable, Content: View>: View {
-    private let data: [Item]
-    private let content: (Item) -> Content
+    // MARK: Lifecycle
 
     public init(data: [Item], @ViewBuilder content: @escaping (Item) -> Content) {
         self.data = data
         self.content = content
     }
 
+    // MARK: Public
+
     public var body: some View {
         List(data) { item in
             content(item)
         }
     }
+
+    // MARK: Private
+
+    private let data: [Item]
+    private let content: (Item) -> Content
 }
 
 public struct GeneralRowView<Item: Identifiable, Content: View>: View {
-    @State private var stats: Stats?
-
-    private let item: Item
-    private let content: (Item) -> Content
-    private let loadStats: () async -> Stats
+    // MARK: Lifecycle
 
     public init(item: Item, @ViewBuilder content: @escaping (Item) -> Content, loadStats: @escaping () async -> Stats) {
         self.item = item
         self.content = content
         self.loadStats = loadStats
     }
+
+    // MARK: Public
 
     public var body: some View {
         HStack {
@@ -51,4 +55,12 @@ public struct GeneralRowView<Item: Identifiable, Content: View>: View {
             stats = await loadStats()
         }
     }
+
+    // MARK: Private
+
+    @State private var stats: Stats?
+
+    private let item: Item
+    private let content: (Item) -> Content
+    private let loadStats: () async -> Stats
 }
