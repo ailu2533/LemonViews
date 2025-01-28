8a4230c286e0452fa2bc627b98d83ceb8962950f
ailu <im.ailu@outlook.com>
2025-01-28 16:05:32 +0800

    update

--- Sources/LemonViews/SettingsView/SettingsLabelView.swift
+++ Sources/LemonViews/SettingsView/SettingsLabelView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 public struct SettingsLabelView: View {
     // MARK: Lifecycle
 
-    public init(systemName: String, text: LocalizedStringKey, foregroundColor: Color = .white, backgroundColor: Color) {
+    public init(
+        systemName: String,
+        text: LocalizedStringKey,
+        foregroundColor: Color = .white,
+        backgroundColor: Color
+    ) {
         self.systemName = systemName
         self.text = text
         self.foregroundColor = foregroundColor
@@ -21,11 +26,16 @@ public struct SettingsLabelView: View {
 
     public var body: some View {
         HStack {
-            SettingIconView(icon: .system(icon: systemName, foregroundColor: foregroundColor, backgroundColor: backgroundColor))
+            SettingIconView(
+                icon: .system(
+                    icon: systemName,
+                    foregroundColor: foregroundColor,
+                    backgroundColor: backgroundColor
+                )
+            )
 
             Text(text)
                 .padding(.leading, 5)
-                .foregroundStyle(Color(.netureBlack))
         }
     }
 
@@ -38,5 +48,5 @@ public struct SettingsLabelView: View {
 }
 
 #Preview {
-    SettingsLabelView(systemName: "tshirt", text: "衣服", backgroundColor: .blue)
+    SettingsLabelView(systemName: "tshirt.fill", text: "衣服", backgroundColor: .blue)
 }
